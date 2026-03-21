39e934028baaf46f9ae765fe81b807a17480929b
FeCordoba <fecordoba2@gmail.com>
2026-03-21 14:40:46 -0300

    Add files via upload
    En esta vez, hice modificaciones en el codigo guiandome con ia para poder respetar la pep 8 lo mas posible

--- questions.py
+++ questions.py
@@ -1,28 +1,30 @@
 import string
 import random
-puntos=0 # dependiendo la palabra que ingrese el usuario se le restaran o sumaran puntos 
+puntos = 0 # Dependiendo la palabra que ingrese el usuario se le restaran o sumaran puntos 
 
-menu={
+menu = {
     "lenguajes":["python"],
-    "conceptos_basicos":['bucle','variable','programa','funcion'],
-    "tipos_datos":['entero','lista','cadena']
-} #cree el diccionario solicitado 
+    "conceptos_basicos":['bucle', 'variable', 'programa', 'funcion'],
+    "tipos_datos":['entero', 'lista', 'cadena']
+} #  Cree el diccionario solicitado 
 
-print('categorias: ')
+print('Categorias: ')
 for categoria in menu:
-           print(categoria, '-')
+    print(categoria, '-')
 
-cat=input('ingresa la categoria elegida: ')
+cat = input('Ingresa la categoria elegida: ')
 
 if cat in menu:
-    print('la categoria es:',cat)
+    print('La categoria es:', cat)
     for pal in menu[cat]:
         print(pal)
-else: print('categoria no valida')
+else: 
+    print('Categoria no valida, ¡Reinicia el juego para jugar!')
+    exit()
 
 
-word = random.sample(menu[cat],1)[0] #usando random.sample debo mandarle la coleccion, cuantos elem quiero de esa coleccion y agregar [0] para sacar a ese elem de la lista, ya que esta funcion devuelve una lista y no un string como necesitamos 
-guessed = []
+word = random.sample(menu[cat], 1)[0] #  Con random.sample mando coleccion, cuantos elem quiero de esa y agrego [0] para sacar a ese elem de la lista, ya que esta funcion devuelve una lista y no un string como necesitamos 
+guessed = []                          #  Se que fue un comentario demasiado largo
 attempts = 6
 
 print("¡Bienvenido al Ahorcado!")
@@ -46,11 +48,11 @@ while attempts > 0:
     print(f"Letras usadas: {', '.join(guessed)}")
 
 
-    cadena=string.ascii_lowercase
+    cadena = string.ascii_lowercase
     letter = input("Ingresá una letra: ")
 
-    if (letter not in cadena or len(letter)!=1): # modificacion por si ingresa un char invalido o mas de uno 
-        print('entrada no valida')
+    if (letter not in cadena or len(letter)!= 1): #  Modificacion por si ingresa un char invalido o mas de uno 
+        print('Entrada no valida')
         continue
     elif letter in guessed:
         print("Ya usaste esa letra.")
@@ -66,7 +68,7 @@ while attempts > 0:
     print()
 
 else:
-    puntos=0
+    puntos = 0
     print(f"¡Perdiste! La palabra era: {word}")
 
-print('puntos ganados: ',(puntos)) 
+print('Puntos ganados: ',(puntos)) 
